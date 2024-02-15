6a896ab96b2878c2c525cd1a2aca3443977022b9
Heloisa Carbone <heloisa.carbone@travelperk.com>
2024-02-15 10:30:39 +0100

    lint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,8 +7,9 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
-
 """aux function to create users"""
+
+
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
